d5b2d3b5a392a4ed36e9483d863de61f0bd11c16
aiquanttech <sundahu2@163.com>
2017-05-25 13:48:51 +0800

    update

--- js/main.js
+++ js/main.js
@@ -6,17 +6,16 @@ jQuery(function($) {
 			var $anchor = $(this);
 			var nav = $($anchor.attr('href'));
 			if (nav.length) {
-			$('html, body').stop().animate({				
-				scrollTop: $($anchor.attr('href')).offset().top				
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
 			}, 1500, 'easeInOutExpo');
 			
 			event.preventDefault();
 			}
 		});
-						
-	});	
-	
-	
+
+	});
+
 
 	//Initiat WOW JS
 	new WOW().init();
@@ -56,25 +55,4 @@ jQuery(function($) {
 			$("html, body").animate({ scrollTop: 0 }, 1000);
 				return false;
 		});
-        
-    //Google Map
-    var get_latitude = $('#google-map').data('latitude');
-    var get_longitude = $('#google-map').data('longitude');
-
-    function initialize_google_map() {
-        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
-        var mapOptions = {
-            zoom: 14,
-            scrollwheel: false,
-            center: myLatlng
-        };
-        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map
-        });
-    }
-    google.maps.event.addDomListener(window, 'load', initialize_google_map);
-	
-	
 });
